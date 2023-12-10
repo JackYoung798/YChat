c4be361285721dc2d133a34fe55f906781eb4b3e
Jack Young <jackyoung0798@gmail.com>
2023-12-10 16:01:07 +0800

    路由模块修改

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const router = express.Router(); // 注册路由 
 const userRouter = require('./user'); // 引入user路由模块
-const service = require('../services/userService');
 
-// 用户注册
-router.post('/reg', service.reg);
-// 用户登录
-router.post('/log', service.log);
-//退出登录
-router.post('/exit',service.exit)
-//基本资料修改
-router.post('/update', service.update)
-//头像修改
-router.post('/avatar', service.avatar)
+router.use('/user', userRouter); // 注入用户路由模块
 
 module.exports = router;
-
-
-
-
-
-
