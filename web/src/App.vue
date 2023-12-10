5d770ab53683b8cf07a234ea2b78095ca1039fa2
Jack Young <jackyoung0798@gmail.com>
2023-12-10 23:25:13 +0800

    feat🥳：添加过渡动效

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="body-container">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="router" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>  
   </div>
 </template>
 
 <style scoped>
-html, body {
-  height: 100%;
-  overflow: auto;
-  margin: 0;
+.router-enter-active,
+.router-leave-active {
+  transition: all 0.3s ease;
 }
-html{
-  overflow-y: auto;
+.router-enter-from,
+.router-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
 }
 </style>
